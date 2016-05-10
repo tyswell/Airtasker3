66aea63e91bd6e0dc377bfae8eacc7e87c3b98ac
tyswell <deksentys@gmail.com>
2016-05-10 13:07:25 +0700

    New changelist

---
+++ app/src/main/java/tys/com/airtasker3/constant/Page.java
@@ -0,0 +1,14 @@
+package tys.com.airtasker3.constant;
+
+/**
+ * Created by chokechaic on 5/10/2016.
+ */
+public class Page {
+
+    public static final int CREATETASK_REQ_CODE = 1;
+
+    public static final int CREATETASK_DETAIL_PAGE = 0;
+    public static final int CREATETASK_LOCATION_PAGE = 1;
+    public static final int CREATETASK_BUDGET_PAGE = 2;
+
+}
